dfacc85d63c13f60890154bd18b9037bd51c3695
deepak01mehta <deepakmehta8668572175@gmail.com>
2026-07-08 14:23:41 +0530

    Learn nested loops and number with decresing way  printing

---
+++ day 1/numberpattern.py
@@ -0,0 +1,10 @@
+row =int(input("Enter the number of row"))
+
+for i in range(1,row+1):
+    for j in range(1,i+1):
+        print(j, end="")
+
+    for k in range(i-1,0,-1):
+        print(k,end="")
+
+    print()
